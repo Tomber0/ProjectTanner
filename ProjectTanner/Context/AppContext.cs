3577109a2209a3183d7b89aea70a7cdd0dd9ee3e
llzai <llzaicev@yandex.ru>
2024-03-21 19:30:01 +0300

    added models

---
+++ ProjectTanner/Context/AppContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectTanner.Models;
+
+namespace ProjectTanner.Context
+{
+    public class AppContext:DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!; 
+        public DbSet<Models.Task> Tasks { get; set; } = null!; 
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseMySql("server=.;user=root;password=123456789;database=usersdb;",
+                new MySqlServerVersion(new Version(8, 0, 25)));
+        }
+    }
+}
---
+++ ProjectTanner/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectTanner.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TId { get; set; }
+        public List<Task> Tasks { get; set; } = new();
+
+    }
+}
